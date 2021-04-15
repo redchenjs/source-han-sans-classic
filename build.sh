dc6721daadf8b58028a3145232b67cdf0420b231
Jack Chen <redchenjs@live.com>
2021-04-15 15:17:43 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p OTF
 
-for dir in ExtraLight Light Normal Regular Medium Bold; do
+for dir in ExtraLight Light Normal Regular Medium Bold Heavy; do
   cd "$dir/OTC"
   makeotf -f cidfont.ps.OTC.CL -omitMacNames -ff features.OTC.CL -fi cidfontinfo.OTC.CL -mf ../../FontMenuNameDB -r -nS -cs 3 -ch ../../UniSourceHanSansCL-UTF32-H -ci ../../SourceHanSans_CL_sequences.txt ; tx -cff +S -no_futile cidfont.ps.OTC.CL CFF.OTC.CL ; sfntedit -a CFF=CFF.OTC.CL "SourceHanSansC-$dir.otf"
   mv "SourceHanSansC-$dir.otf" ../../OTF
@@ -10,4 +10,4 @@ for dir in ExtraLight Light Normal Regular Medium Bold; do
   cd ../../
 done
 
-zip source-han-sans-classic-otf.zip OTF/*
+zip source-han-sans-classic-otf.zip LICENSE.txt OTF/*
